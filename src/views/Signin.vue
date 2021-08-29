6034c1b330972009b63a51d1c1f1e99b4ee3d2d2
Ye Myint <kreatorye@gmail.com>
2021-08-29 22:21:51 +0800

    Added firebase settings

---
+++ src/views/Signin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <label for="password">Password</label>
+    <Password id="password" v-model="value" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data() {
+    return {
+      value: null,
+    }
+  },
+  methods: {},
+}
+</script>
